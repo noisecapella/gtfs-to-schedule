5880fb13a02d8d52f6f3d620cb0c4b2699204d61
George Schneeloch <bostonbusmap@gmail.com>
2013-03-11 00:51:22 -0400

    times should be in order, but services still need to be grouped by dates

--- make_schedule.py
+++ make_schedule.py
@@ -3,18 +3,7 @@
 import argparse
 import os
 import csv
-from collections import namedtuple
-
-def read_map(path, key):
-    ret = {}
-
-    with open(path) as f:
-        reader = csv.DictReader(f)
-
-        for row in reader:
-            ret[row[key]] = row
-
-    return ret
+from collections import defaultdict
 
 def parse_time(s):
     """Returns seconds from beginning of day. May go into tomorrow slightly"""
@@ -31,6 +20,72 @@ def parse_time(s):
 
     return second + 60*minute + 60*60*hour + 24*60*60*day
 
+def time_to_string(time):
+    """Seconds from beginning of day to string"""
+
+    hour = time / (60*60)
+    time -= hour*60*60
+
+    minute = time/60
+    time -= minute*60
+
+    second = time
+
+    return "%02d:%02d:%02d" % (hour,minute,second)
+
+class Schedule:
+    """A compressed schedule"""
+    def __init__(self):
+        self.pieces = []
+        self.trip = None
+
+    def add_time(self, arrival_time):
+        i = 0
+        for piece in self.pieces:
+            start_time, inc, count = piece
+
+            if arrival_time <= start_time:
+                i -= 1
+                break
+            
+            i += 1
+
+        if i < 0:
+            self.pieces.insert(0, (arrival_time, 0, 0))
+        elif i >= len(self.pieces):
+            self.pieces.append((arrival_time, 0, 0))
+        else:
+            piece = self.pieces[i]
+            start_time, inc, count = piece
+            
+            diff = arrival_time - start_time
+            if inc == 0:
+                self.pieces[i] = start_time, diff, 1
+            elif diff % inc == 0:
+                self.pieces[i] = start_time, inc, count+1
+            else:
+                self.pieces.insert(i+1, (arrival_time, 0, 0))
+
+    def __str__(self):
+        ret = ""
+
+        for piece in self.pieces:
+            start_time, inc, count = piece
+            ret += "        start at %s, repeat every %d minutes %d times\n" % (time_to_string(start_time), inc/60, count) 
+
+        return ret
+
+def read_map(path, key):
+    ret = {}
+
+    with open(path) as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            ret[row[key]] = row
+
+    return ret
+
 def parse(path):
     print "reading routes..."
     routes = read_map(os.path.join(path, "routes.txt"), "route_id")
@@ -38,10 +93,13 @@ def parse(path):
     stops = read_map(os.path.join(path, "stops.txt"), "stop_id")
     print "reading trips..."
     trips = read_map(os.path.join(path, "trips.txt"), "trip_id")
+    print "reading calendar..."
+    calendar = read_map(os.path.join(path, "calendar.txt"), "service_id")
+    
 
     print "reticulating splines..."
     # mapping of (stop, direction) to list of (start_time, increment, count)
-    schedule = {}
+    schedule = defaultdict(Schedule)
 
     with open(os.path.join(path, "stop_times.txt")) as f:
         reader = csv.DictReader(f)
@@ -49,27 +107,24 @@ def parse(path):
         for row in reader:
             if row["stop_sequence"] != "1":
                 continue
-            key = row["stop_id"], trips[row["trip_id"]]["direction_id"]
+            trip = row["trip_id"]
+            key = row["stop_id"], trips[trip]["trip_headsign"], trips[trip]["route_id"], trips[trip]["service_id"]
 
-            lst = schedule.get(key, [])
-            schedule[key] = lst
+            sched = schedule[key]
+            sched.trip = trip
 
             arrival_time = parse_time(row["arrival_time"])
-            if len(lst) >= 1:
-                start_time, inc, count = lst[-1]
-                diff = arrival_time - start_time
-                if inc == 0:
-                    lst[-1] = start_time, diff, 1
-                elif diff % inc == 0:
-                    lst[-1] = start_time, inc, count+1
-                else:
-                    lst.append((arrival_time, 0, 0))
-            else:
-                lst.append((arrival_time, 0, 0))
-            
+            sched.add_time(arrival_time)
+        
+    # mapping of route to list of mappings from directions to a compressed schedule
+    ret = defaultdict(lambda: defaultdict(dict))
 
-    return schedule
+    for key, sched in schedule.iteritems():
+        stop, direction, route, service = key
 
+        ret[route][direction][service] = sched
+
+    return ret
 
 def main():
     parser = argparse.ArgumentParser(description='Parses GTFS data into general schedule')
@@ -83,7 +138,13 @@ def main():
 
     schedule = parse(args.path)
 
-    print schedule
+    for route, direction_map in schedule.iteritems():
+        print "Route: %s" % route
+        for direction, service_map in direction_map.iteritems():
+            print "    Direction: %s" % direction
+            for service, sched in service_map.iteritems():
+                print "    Service: %s" % service
+                print "    Schedule: %s" % str(sched)
 
 if __name__ == "__main__":
     main()
