4489c8eb43cd6015e285ae8ba31359a3d04c188b
George Schneeloch <bostonbusmap@gmail.com>
2013-05-03 19:25:18 -0400

    Add serialization structure

---
+++ box.py
@@ -0,0 +1,26 @@
+__author__ = 'schneg'
+
+import binascii
+import struct
+class Box:
+    """Usage:
+
+    box = Box()
+
+    box.add_int(3)
+    box.add_int(-4)
+
+    print("INSERT INTO table VALUES (%s)" % box.get_blob_string()")
+    """
+    def __init__(self):
+        self.bytes = "" # this is hex
+
+    def add_int(self, x):
+        self.bytes += binascii.b2a_hex(struct.pack('>i', x))
+
+    def add_float(self, f):
+        self.bytes += binascii.b2a_hex(struct.pack('>f', f))
+
+    def get_blob_string(self):
+        return "X'" + self.bytes + "'"
+
