b4fbdcdf4b8376960bd73c0ec0023ade72c508ce
George Schneeloch <bostonbusmap@gmail.com>
2013-05-21 19:13:58 -0400

    Rewriting

--- make_database.py
+++ make_database.py
@@ -3,12 +3,15 @@
 from make_schedule import parse
 import argparse
 import os
+import csv
+from collections import defaultdict
 
 from box import Box
 
 from make_schedule import (
     make_days,
     make_days_hash,
+    parse_time,
     )
 
 from schedules import (
@@ -17,140 +20,73 @@ from schedules import (
 
 )
 
-def print_service_table(f, calendar):
-    f.write("CREATE TABLE IF NOT EXISTS service (id INTEGER PRIMARY KEY, "
-            "days_of_week INTEGER, start_date INTEGER, end_date INTEGER);\n")
-    service_ids = {}
-    count = 0
-    for service_id, row in calendar.items():
-        arr = make_days(calendar, service_id)
-        h = make_days_hash(arr)
-
-        f.write("INSERT INTO service VALUES (%d, %d, %d, %d);\n" %
-                (count, h, int(row["start_date"]), int(row["end_date"])))
-        service_ids[row["service_id"]] = count
-        count += 1
-    return service_ids
-
-def print_schedule_table(f, schedule, service_ids, direction_ids):
-    f.write("CREATE TABLE IF NOT EXISTS schedule (id INTEGER PRIMARY KEY, direction_id INTEGER, service_id INTEGER);\n")
-
-    count = 0
-    schedule_ids = {}
-    for route, direction_map in schedule.items():
-        for direction, service_map in direction_map.items():
-            for service, sched in service_map.items():
-                service_id = service_ids[service]
-                direction_id = direction_ids[direction]
-
-                f.write("INSERT INTO schedule VALUES (%d, %d, %d);\n" %
-                        (count, direction_id, service_id))
-                schedule_ids[(route, direction, service)] = count
-                count += 1
-    return schedule_ids
-
 def escaped(s):
     return s.replace("'", "''")
 
 
-def print_direction_table(f, schedule):
-    f.write("CREATE TABLE IF NOT EXISTS gtfs_directions (id INTEGER PRIMARY KEY, title STRING);\n")
-    count = 0
-    direction_ids = {}
-    for route, direction_map in schedule.items():
-        for direction in direction_map.keys():
-            direction_ids[direction] = count
+def print_stop_times_table(f, stop_times):
+    for trip_id, stop_times_map in stop_times.items():
+        for stop_id, times_tuple in stop_times_map.items():
+            arrival_seconds, depart_seconds = times_tuple
 
-            f.write("INSERT INTO gtfs_directions VALUES (%d, '%s');\n" % (count, escaped(direction)))
+
+
+def make_index_map(array):
+    ret = {}
+    for i, item in enumerate(array):
+        ret[item] = i
+    return ret
+
+def write_stop_ids_table(out_file, csv_path):
+    ret = {}
+    out_file.write("CREATE TABLE stop_ids (id INTEGER PRIMARY KEY, stop_id STRING);\n")
+    count = 0
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file)
+
+        header = make_index_map(next(reader))
+        for row in reader:
+            stop_id = row[header["stop_id"]]
+            out_file.write("INSERT INTO stop_ids VALUES (%d, '%s');\n" % (count, stop_id))
+            if stop_id in ret:
+                raise Exception("Duplicate stop: %s" % stop_id)
+            ret[stop_id] = count
             count += 1
+    return ret
 
-    return direction_ids
-
-
-def print_stop_schedule_table(f, schedule, schedule_ids):
-    f.write("CREATE TABLE IF NOT EXISTS stop_schedule (id INTEGER PRIMARY KEY, service_id INTEGER, stop_id STRING);\n")
+def write_trip_ids_table(out_file, csv_path):
+    ret = {}
+    out_file.write("CREATE TABLE trip_ids (id INTEGER PRIMARY KEY, trip_id STRING);\n")
     count = 0
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file)
 
-    stop_schedule_ids = {}
-    for route, direction_map in schedule.items():
-        for direction, service_map in direction_map.items():
-            for service, sched in service_map.items():
-                for stop in sched.stops:
-                    sched_key = (route, direction, service)
-                    f.write("INSERT INTO stop_schedule VALUES (%d, %d, '%s');\n" %
-                            (count, schedule_ids[sched_key], escaped(stop)))
-                    stop_schedule_ids[(route, direction, service, stop)] = count
+        header = make_index_map(next(reader))
+        for row in reader:
+            trip_id = row[header["trip_id"]]
+            out_file.write("INSERT INTO trip_ids VALUES (%d, '%s');\n" % (count, trip_id))
+            if trip_id in ret:
+                raise Exception("Duplicate trip: %s" % trip_id)
+            ret[trip_id] = count
+            count += 1
+    return ret
 
-                    count += 1
+def write_stop_times_table(out_file, csv_path, stop_id_map, trip_id_map):
+    out_file.write("CREATE TABLE stop_times (trip_id INTEGER, stop_id INTEGER, "
+                   "arrival_seconds INTEGER, depart_seconds INTEGER);\n")
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file)
 
-    return stop_schedule_ids
+        header = make_index_map(next(reader))
+        for row in reader:
+            trip_id = row[header["trip_id"]]
+            stop_id = row[header["stop_id"]]
+            arrival_seconds = parse_time(row[header["arrival_time"]])
+            departure_seconds = parse_time(row[header["departure_time"]])
 
-
-def print_stop_schedule_row_table(f, schedule, stop_schedule_ids):
-    f.write("CREATE TABLE IF NOT EXISTS stop_schedule_row (stop_schedule_id"
-            " INTEGER, start_time INTEGER, repeats INTEGER, diff INTEGER);\n")
-
-    count = 0
-    for route, direction_map in schedule.items():
-        for direction, service_map in direction_map.items():
-            for service, sched in service_map.items():
-                for stop in sched.stops:
-                    stop_schedule_or_diff = sched.schedules[stop]
-                    if type(stop_schedule_or_diff) == StopSchedule:
-                        stop_schedule_id = stop_schedule_ids[(route, direction, service, stop)]
-                        for piece in stop_schedule_or_diff.pieces:
-                            arrival_time, diff, repeat_count = piece
-                            f.write("INSERT INTO stop_schedule_row VALUES (%d, %d, %d, %d);\n" %
-                                    (stop_schedule_id, arrival_time, diff, repeat_count))
-                            count += 1
-                    # else it's a duplicate, handle in print_schedule_duplicate_table
-
-
-def print_stop_schedule_duplicate_table(f, schedule, stop_schedule_ids, schedule_ids, diff_ids):
-    f.write("CREATE TABLE IF NOT EXISTS stop_schedule_duplicate (id INTEGER PRIMARY KEY,"
-            " schedule_id INTEGER, diff INTEGER);\n")
-    # TODO: whole schedule for first stop is exactly d seconds from this stop
-    count = 0
-    for route, direction_map in schedule.items():
-        for direction, service_map in direction_map.items():
-            for service, sched in service_map.items():
-                for stop in sched.stops:
-                    stop_schedule_or_diff = sched.schedules[stop]
-                    if type(stop_schedule_or_diff) == tuple:
-                        prev_sched, diff = stop_schedule_or_diff
-                        stop_schedule_id = stop_schedule_ids[(route, direction, service, stop)]
-                        schedule_id = schedule_ids[(route, direction, service)]
-                        diff_id = diff_ids[tuple(diff)]
-                        f.write("INSERT INTO stop_schedule_duplicate VALUES (%d, %d, %d);\n" %
-                                (stop_schedule_id, schedule_id, diff_id))
-                    # else it's not a duplicate
-
-
-def print_diff_table(f, schedule):
-    diff_ids = {}
-    count = 0
-    f.write("CREATE TABLE IF NOT EXISTS diff (diff_id INTEGER, diff BLOB);\n")
-    for route, direction_map in schedule.items():
-        for direction, service_map in direction_map.items():
-            for service, sched in service_map.items():
-                for stop in sched.stops:
-                    stop_schedule_or_diff = sched.schedules[stop]
-                    if type(stop_schedule_or_diff) == tuple:
-                        prev_sched, diff = stop_schedule_or_diff
-                        diff_key = tuple(diff)
-                        if diff_key not in diff_ids:
-                            diff_ids[diff_key] = count
-                            box = Box()
-                            box.add_ints(diff)
-                            count += 1
-                            f.write("INSERT INTO diff VALUES (%d, %s);\n" %
-                                    (count, box.get_blob_string()))
-    return diff_ids
-
-
-def print_trip_table(f, schedule):
-    f.write("CREATE TABLE IF NOT EXISTS trips (id INTEGER PRIMARY KEY, start_time INTEGER);\n")
-    #TODO
+            out_file.write("INSERT INTO stop_times VALUES (%d, %d, %d, %d);\n" % (
+                trip_id_map[trip_id], stop_id_map[stop_id], arrival_seconds, departure_seconds
+            ))
 
 
 def main():
@@ -170,15 +106,9 @@ def main():
 
     with open(args.output_file, "w") as f:
         f.write("BEGIN TRANSACTION;\n")
-        schedule, trips, calendar = parse(args.path)
-
-        service_ids = print_service_table(f, calendar)
-        direction_ids = print_direction_table(f, schedule)
-        schedule_ids = print_schedule_table(f, schedule, service_ids, direction_ids)
-        stop_schedule_ids = print_stop_schedule_table(f, schedule, schedule_ids)
-        print_stop_schedule_row_table(f, schedule, stop_schedule_ids)
-        diff_ids = print_diff_table(f, schedule)
-        print_stop_schedule_duplicate_table(f, schedule, stop_schedule_ids, schedule_ids, diff_ids)
+        trip_ids_map = write_trip_ids_table(f, os.path.join(args.path, "trips.txt"))
+        stop_ids_map = write_stop_ids_table(f, os.path.join(args.path, "stops.txt"))
+        write_stop_times_table(f, os.path.join(args.path, "stop_times.txt"), stop_ids_map, trip_ids_map)
 
         f.write("END TRANSACTION;\n")
 
